d0758e8ef0cc172f82a33e507a8c61a38d861b4a
zhaori96 <nleonascimento96@gmail.com>
2024-06-11 11:14:03 -0300

    Add new constructor function to Size struct and new methods

--- size.go
+++ size.go
@@ -14,9 +14,39 @@ func NewSize(width, height float64) Size {
 	}
 }
 
+func NewSizeWithPadding(width, height float64, padding Margin) Size {
+	size := Size{
+		Width:  width,
+		Height: height,
+	}
+
+	return size.WithPadding(padding)
+}
+
 func (s *Size) ToRect() *gopdf.Rect {
 	return &gopdf.Rect{
 		W: s.Width,
 		H: s.Height,
+	}
+}
+
+func (s Size) WithPadding(padding Margin) Size {
+	return Size{
+		Width:  s.Width - padding.Left - padding.Right,
+		Height: s.Height - padding.Top - padding.Bottom,
+	}
+}
+
+func (s Size) WithHorizontalPadding(left float64, right float64) Size {
+	return Size{
+		Width:  s.Width - left - right,
+		Height: s.Height,
+	}
+}
+
+func (s Size) WithVerticalPadding(top float64, bottom float64) Size {
+	return Size{
+		Width:  s.Width,
+		Height: s.Height - top - bottom,
 	}
 }
