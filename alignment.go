9cfade0ca73248228842b6ca47dc3fc5ac1e2211
zhaori96 <nleonascimento96@gmail.com>
2024-06-11 11:12:11 -0300

    Update Alignments values

--- alignment.go
+++ alignment.go
@@ -3,10 +3,11 @@ package main
 type Alignment int
 
 const (
-	LeftAlignment Alignment = iota + 1<<16
-	RightAlignment
-	CenterAlignment
-	MiddleAlignment
-	TopAlignment
-	BottomAlignment
+	LeftAlignment Alignment = 8
+	RightAlignment Alignment = 2
+	CenterAlignment Alignment = 16
+	MiddleAlignment Alignment = 32
+	TopAlignment Alignment = 4
+	BottomAlignment Alignment = 1
 )
+
